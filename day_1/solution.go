15b0875cd31505ef96d5a7e547e8629f3ac0b680
Tch1b0 <jpour3006@gmail.com>
2022-11-01 11:57:21 +0100

    partially solve day 1

--- day_1/solution.go
+++ day_1/solution.go
@@ -1,7 +1,40 @@
 package main
 
-func part1() {
+import (
+	"fmt"
 
+	"github.com/tch1b0/polaris/input"
+)
+
+func getInput() string {
+	return input.Process("day_1/input.txt", func(in string) string {
+		newStr := ""
+		for _, chr := range in {
+			// only accept runes that digits
+			if chr >= '0' && chr <= '9' {
+				newStr += string(chr)
+			}
+		}
+		return newStr
+	})
+}
+
+func part1() {
+	prev := ' '
+	sum := 0
+	for _, digit := range getInput() {
+		if prev != ' ' {
+			if prev == digit {
+				sum += int(digit) - int('0')
+				prev = ' '
+			}
+			prev = digit
+		} else {
+			prev = digit
+		}
+	}
+
+	fmt.Printf("PART 1: Sum is %d\n", sum)
 }
 
 func part2() {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tch1b0/aoc2017
 
 go 1.19
+
+require github.com/tch1b0/polaris v0.0.0-20221101091244-d9d63e4d853f
